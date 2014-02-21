29500c4701a61d4aab9241e6f12f98ed8ec7ae51
Dan Schaefer <dan@schaeferzone.net>
2014-02-21 14:58:10 -0500

    Implementing a workaround for accessing multidimensional hash elements

--- manifests/wordpress/plugins.pp
+++ manifests/wordpress/plugins.pp
@@ -1,15 +1,17 @@
 # Class lieutdan13::wordpress::plugins
 class lieutdan13::wordpress::plugins {
 
-    if $::wordpress::options['plugins']['google-analytics-for-wordpress'] != '' {
+    $plugin_options = $::wordpress::options['plugins']
+
+    if $plugin_options['google-analytics-for-wordpress'] != '' {
         lieutdan13::wordpress::plugin { 'google-analytics-for-wordpress':
-            ensure  => $::wordpress::options['plugins']['google-analytics-for-wordpress'] ? {
+            ensure  => $plugin_options['google-analytics-for-wordpress'] ? {
                 /(false|absent)/ => absent,
                 default          => present,
             },
-            version =>  $::wordpress::options['plugins']['google-analytics-for-wordpress'] ? {
-                /(true|latest)/    => '4.3.5', #TODO Put this in a params class
-                default => $::wordpress::options['plugins']['google-analytics-for-wordpress'],
+            version =>  $plugin_options['google-analytics-for-wordpress'] ? {
+                /(true|latest)/  => '4.3.5', #TODO Put this in a params class
+                default          => $plugin_options['google-analytics-for-wordpress'],
             },
         }
     }

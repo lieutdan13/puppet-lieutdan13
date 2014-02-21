0f6d2cbb1c7ac7569c703a49ac8caa81c3d4c84e
Dan Schaefer <dan@schaeferzone.net>
2014-02-21 13:10:29 -0500

    Adding Google Analytics to the list of possible plugins

--- manifests/wordpress/plugins.pp
+++ manifests/wordpress/plugins.pp
@@ -1,3 +1,16 @@
 # Class lieutdan13::wordpress::plugins
 class lieutdan13::wordpress::plugins {
+
+    if $::wordpress::options['plugins']['google-analytics-for-wordpress'] != '' {
+        lieutdan13::wordpress::plugin { 'google-analytics-for-wordpress':
+            ensure  => $::wordpress::options['plugins']['google-analytics-for-wordpress'] ? {
+                /(false|absent)/ => absent,
+                default          => present,
+            },
+            version =>  $::wordpress::options['plugins']['google-analytics-for-wordpress'] ? {
+                /(true|latest)/    => '4.3.5', #TODO Put this in a params class
+                default => $::wordpress::options['plugins']['google-analytics-for-wordpress'],
+            },
+        }
+    }
 }

9dc14496c2283d4dba2e627eabb410b02a942178
Dan Schaefer <dan@schaeferzone.net>
2014-02-21 15:20:22 -0500

    Enabling the installation of the plugin

--- manifests/wordpress/plugin.pp
+++ manifests/wordpress/plugin.pp
@@ -22,15 +22,30 @@ define lieutdan13::wordpress::plugin (
         default => $source_url,
     }
 
-#    puppi::netinstall { "wp-plugin ${name}":
-#        url             => $real_source_url,
-#        destination_dir => $real_destination_dir,
-#        extracted_dir   => $real_extracted_dir
-#    }
+    case $ensure {
+        'present': {
+            puppi::netinstall { "wp-plugin ${name}":
+                url             => $real_source_url,
+                destination_dir => $real_destination_dir,
+                extracted_dir   => $real_extracted_dir
+            }
+        }
 
-    notify { "wp-plugin ${name} \$real_source_url": message => $real_source_url, loglevel => debug, }
-    notify { "wp-plugin ${name} \$ensure": message => $ensure, loglevel => debug, }
-    notify { "wp-plugin ${name} \$version": message => $version, loglevel => debug, }
-    notify { "wp-plugin ${name} \$real_destination_dir": message => $real_destination_dir, loglevel => debug, }
-    notify { "wp-plugin ${name} \$real_extracted_dir": message => $real_extracted_dir, loglevel => debug, }
+        'absent': {
+            file { "wp-plugin ${name}":
+                ensure  => absent,
+                force   => true,
+                path    => "${real_destination_dir}/${real_extracted_dir}",
+                recurse => true,
+            }
+        }
+
+        default: { }
+    }
+
+#    notify { "wp-plugin ${name} \$real_source_url": message => $real_source_url, loglevel => debug, }
+#    notify { "wp-plugin ${name} \$ensure": message => $ensure, loglevel => debug, }
+#    notify { "wp-plugin ${name} \$version": message => $version, loglevel => debug, }
+#    notify { "wp-plugin ${name} \$real_destination_dir": message => $real_destination_dir, loglevel => debug, }
+#    notify { "wp-plugin ${name} \$real_extracted_dir": message => $real_extracted_dir, loglevel => debug, }
 }
--- manifests/wordpress/plugins.pp
+++ manifests/wordpress/plugins.pp
@@ -3,16 +3,15 @@ class lieutdan13::wordpress::plugins {
 
     $plugin_options = $::wordpress::options['plugins']
 
-    if $plugin_options['google-analytics-for-wordpress'] != '' {
-        lieutdan13::wordpress::plugin { 'google-analytics-for-wordpress':
-            ensure  => $plugin_options['google-analytics-for-wordpress'] ? {
-                /(false|absent)/ => absent,
-                default          => present,
-            },
-            version =>  $plugin_options['google-analytics-for-wordpress'] ? {
-                /(true|latest)/  => '4.3.5', #TODO Put this in a params class
-                default          => $plugin_options['google-analytics-for-wordpress'],
-            },
-        }
+    lieutdan13::wordpress::plugin { 'google-analytics-for-wordpress':
+        ensure  => $plugin_options['google-analytics-for-wordpress'] ? {
+            /(false|absent)/ => absent,
+            ''               => absent,
+            default          => present,
+        },
+        version =>  $plugin_options['google-analytics-for-wordpress'] ? {
+            /(true|latest)/  => '4.3.5', #TODO Put this in a params class
+            default          => $plugin_options['google-analytics-for-wordpress'],
+        },
     }
 }

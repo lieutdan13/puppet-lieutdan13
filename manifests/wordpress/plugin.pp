b0473fd8f077295918447b761fe6e075dbbfad67
Dan Schaefer <dan@schaeferzone.net>
2014-02-23 10:15:28 -0500

    Removing debug code

--- manifests/wordpress/plugin.pp
+++ manifests/wordpress/plugin.pp
@@ -43,10 +43,4 @@ define lieutdan13::wordpress::plugin (
 
         default: { }
     }
-
-#    notify { "wp-plugin ${name} \$real_source_url": message => $real_source_url, loglevel => debug, }
-#    notify { "wp-plugin ${name} \$ensure": message => $ensure, loglevel => debug, }
-#    notify { "wp-plugin ${name} \$version": message => $version, loglevel => debug, }
-#    notify { "wp-plugin ${name} \$real_destination_dir": message => $real_destination_dir, loglevel => debug, }
-#    notify { "wp-plugin ${name} \$real_extracted_dir": message => $real_extracted_dir, loglevel => debug, }
 }

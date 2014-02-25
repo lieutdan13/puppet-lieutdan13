d4d67151890e4adc549d8a476aa34135f26bbe01
Dan Schaefer <dan@schaeferzone.net>
2014-02-25 18:33:50 -0500

    Adding advanced-custom-fields plugin

--- manifests/wordpress/plugins.pp
+++ manifests/wordpress/plugins.pp
@@ -3,6 +3,18 @@ class lieutdan13::wordpress::plugins {
 
     $plugin_options = $::wordpress::options['plugins']
 
+    lieutdan13::wordpress::plugin { 'advanced-custom-fields':
+        ensure  => $plugin_options['advanced-custom-fields'] ? {
+            /(false|absent)/ => absent,
+            ''               => absent,
+            default          => present,
+        },
+        version =>  $plugin_options['advanced-custom-fields'] ? {
+            /(true|latest)/  => 'latest-stable', #TODO Put this in a params class
+            default          => $plugin_options['advanced-custom-fields'],
+        },
+    }
+
     lieutdan13::wordpress::plugin { 'google-analytics-for-wordpress':
         ensure  => $plugin_options['google-analytics-for-wordpress'] ? {
             /(false|absent)/ => absent,
